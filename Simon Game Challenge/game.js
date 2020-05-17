a6ba8720c679bc561e52eacbb6f6d3ed75bce197
leonardomorita <leonardomorita2@gmail.com>
2020-05-17 13:38:51 -0300

    Update the Simon game project

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,29 +1,82 @@
 var gamePattern = [];
 var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+var level = 0;
+var running = false;
+
+// Verify when any buttons are clicked.
+$('.btn').click(function (event) {
+    // Get the id of the button that it pressed, using the event.
+    var userChosenColour =  event.target.id; // $(this).attr("id");
+
+    // Add the id's button that it pressed.
+    userClickedPattern.push(userChosenColour);
+    
+
+    // Plays sounds when the user click on the any button.
+    playSound(userChosenColour);
+
+    // Animation when the user click on the any buttons.
+    animatePress(userChosenColour);
+
+    // Call this method, passing in the index of the last answer in the array.
+    checkAnswer(userClickedPattern.length-1);
+});
+
+$(document).keydown(function () {
+    if (!running) {
+        running = true;
+        $("#level-title").text("Level " + level);
+        nextSequence();
+    }
+});
 
 function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
+
     // Random number between 0 and 3
     var randomNumber = Math.floor(Math.random() * 4);
 
+    // Random colour.
     var randomChosenColour = buttonColours[randomNumber];
 
+    // Add the random colour in the game pattern array.
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeOut(250).fadeIn(250);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-
-    // Verify when any buttons are clicked.
-    $('.btn').click(function (event) {
-        // Get the id of the button that it pressed, using the event.
-        var userChosenColour =  event.target.id; // $(this).attr("id");
-
-        userClickedPattern.push(userChosenColour);
-        
-        console.log(userClickedPattern);
-    });
+    playSound(randomChosenColour);    
 }
 
-nextSequence();
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    
+    setTimeout(function () {
+        // Remove class with timer.
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+    // alert("check: " + currentLevel);
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        } 
+    } else {
+        console.log("wrong");
+    }
+}
